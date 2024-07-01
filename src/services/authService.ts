f2efa20edcc4ca919e5d58dc5f177643bed1e3cc
cvrain <cvraindays@outlook.com>
2024-07-01 14:27:55 +0800

    upload a basic framwork

---
+++ src/services/authService.ts
@@ -0,0 +1,12 @@
+import {User} from "../models/userModel";
+
+export class AuthService{
+    async register(user: User){
+    
+    }
+
+    async login(email: User, password: String) {
+        
+    }
+        
+}
